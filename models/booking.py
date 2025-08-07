56ea94c97204aabffc6769f1404aef47b74dcd6f
Muz1357 <muzeenmohammed6@gmail.com>
2025-08-08 01:30:41 +0530

    sprint

--- models/booking.py
+++ models/booking.py
@@ -1,7 +1,6 @@
 from models.db import get_connection
 from datetime import datetime
 
-created_at = datetime.now
 
 class Booking:
     def __init__(self, id=None, listing_id=None, tourist_id=None, check_in=None, check_out=None):
@@ -10,7 +9,6 @@ class Booking:
         self.tourist_id = tourist_id
         self.check_in = check_in
         self.check_out = check_out
-        created_at
 
     def save(self):
         with get_connection() as conn:
@@ -18,5 +16,5 @@ class Booking:
                 cursor.execute("""
                     INSERT INTO booking (listing_id, tourist_id, check_in, check_out)
                     VALUES (%s, %s, %s, %s)
-                """, (self.listing_id, self.tourist_id, self.check_in, self.check_out, created_at))
+                """, (self.listing_id, self.tourist_id, self.check_in, self.check_out))
             conn.commit()
