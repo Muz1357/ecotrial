90a11317f8cbe22546ddb890a79c660d545fbf19
Muz1357 <muzeenmohammed6@gmail.com>
2025-08-14 01:54:36 +0530

    Sprint 2

--- models/trip.py
+++ models/trip.py
@@ -2,25 +2,23 @@ from models.db import get_connection
 from datetime import datetime
 
 class Trip:
-    def __init__(self, id, tourist_id, start_location, end_location, start_date, end_date, total_distance=0, total_co2=0, created_at=None):
+    def __init__(self, id, tourist_id, start_location, end_location, start_date, end_date, created_at=None):
         self.id = id
         self.tourist_id = tourist_id
         self.start_location = start_location
         self.end_location = end_location
         self.start_date = start_date
         self.end_date = end_date
-        self.total_distance = total_distance
-        self.total_co2 = total_co2
         self.created_at = created_at or datetime.utcnow()
 
     @staticmethod
-    def create(tourist_id, start_location, end_location, start_date, end_date, total_distance, total_co2):
+    def create(tourist_id, start_location, end_location, start_date, end_date):
         conn = get_connection()
         cursor = conn.cursor()
         cursor.execute("""
             INSERT INTO trip (tourist_id, start_location, end_location, start_date, end_date, created_at)
-            VALUES (%s,%s,%s,%s,%s,%s,%s,%s)
-        """, (tourist_id, start_location, end_location, start_date, end_date, total_distance, total_co2, datetime.utcnow()))
+            VALUES (%s,%s,%s,%s,%s,%s)
+        """, (tourist_id, start_location, end_location, start_date, end_date, datetime.utcnow()))
         trip_id = cursor.lastrowid
         conn.commit()
         cursor.close()
